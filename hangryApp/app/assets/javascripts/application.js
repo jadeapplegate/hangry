10eb82f75bd8fc7c484c855f4aad76a6ab5018bb
Jade <jade.applegate@gmail.com>
2014-09-06 11:37:17 -0700

    added yelp secret keys

--- hangryApp/app/assets/javascripts/application.js
+++ hangryApp/app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function get_location() {
+  navigator.geolocation.getCurrentPosition(show_map);
+}
+
+function show_map(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  // let's show a map or do something interesting!
+}
