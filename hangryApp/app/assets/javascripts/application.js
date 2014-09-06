45e00e16e8f70d2a6bc3fce52258233aecc0d817
Jade <jade.applegate@gmail.com>
2014-09-06 15:15:02 -0700

    pin renders on map

--- hangryApp/app/assets/javascripts/application.js
+++ hangryApp/app/assets/javascripts/application.js
@@ -14,31 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-function geoFindMe() {
-  var output = document.getElementById("out");
-
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
-
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    window.location.href="/results?lat=" + latitude + "&long=" + longitude
-   
-  };
-
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  };
-
-  output.innerHTML = "<p>Locating…</p>";
-
-  navigator.geolocation.getCurrentPosition(success, error);
-}
-
-$("#hangry").click(geoFindMe);
-
-
